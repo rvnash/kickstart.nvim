0d683a796ec00ad7c9544987b35223226c6a9290
Richard Nash <rvnash@gmail.com>
2025-03-22 11:11:03 -0400

    cleaning

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -69,7 +69,7 @@ return {
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>si', builtin.git_files, { desc = '[S]earch [G]it' })
+      vim.keymap.set('n', '<leader>si', builtin.git_files, { desc = '[S]earch g[I]t' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
